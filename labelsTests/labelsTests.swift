77093092eec57932c0f5b0f21dfb6669ad8d8f84
duszekjk <jacek@duszekjk.com>
2024-12-28 21:10:02 +0100

    Initial Commit

---
+++ labelsTests/labelsTests.swift
@@ -0,0 +1,16 @@
+//
+//  labelsTests.swift
+//  labelsTests
+//
+//  Created by Jacek Kałużny on 28/12/2024.
+//
+
+import Testing
+
+struct labelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
